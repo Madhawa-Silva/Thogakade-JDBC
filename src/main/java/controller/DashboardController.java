9bc1264b220816afeb48e71017b23e1affe77710
Madhawa-Silva <madhawasilva2004@gmail.com>
2026-02-15 14:05:47 +0530

    Added dashboard with anchorpane navigation for customer form

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,55 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class DashboardController {
+
+    @FXML
+    private JFXButton btnCustomer;
+
+    @FXML
+    private JFXButton btnItem;
+
+    @FXML
+    private JFXButton btnOrder;
+
+    @FXML
+    private AnchorPane viewPane;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+        try {
+        URL resource = this.getClass().getResource("/view/customer_form.fxml");
+
+        assert resource != null;
+
+        Parent parent = FXMLLoader.load(resource);
+
+        viewPane.getChildren().clear();
+        viewPane.getChildren().add(parent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
+}
